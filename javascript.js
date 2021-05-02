f492514db821a9664fd38b7229f1136643df9b37
Conor Sayres <csayres@uw.edu>
2021-05-02 16:04:54 -0700

    a

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+
+// Load Data
+var data = d3.json("./data/mjd-59371-sdss-fields.json");
+
+console.log(data[0]);
+
 d3
   .select(".target")  // select the elements that have the class 'target'
   .style("stroke-width", 20) // change their style: stroke width is not equal to 8 pixels
