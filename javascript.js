e67ff9de7322a13b4ae47b422e8d8fa1ee41ba2c
Conor Sayres <csayres@uw.edu>
2021-05-02 15:48:22 -0700

    a

--- javascript.js
+++ javascript.js
@@ -1,18 +1,11 @@
-var clickMe = document.getElementById('click-me');
-clickMe.onclick = function() {
-  if (this.style.backgroundColor) {
-    this.style.backgroundColor = '';
-  } else {
-    this.style.backgroundColor = 'red';
-  }
-}
+d3
+  .select(".target")  // select the elements that have the class 'target'
+  .style("stroke-width", 20) // change their style: stroke width is not equal to 8 pixels
 
-// D3 Selection API. Note: it attaches the
-// callbacks to each element in the selection
 d3.selectAll('.hover-me')
-  .on('mouseover', function() {
-    this.style.backgroundColor = 'yellow';
-  })
-  .on('mouseleave', function() {
-    this.style.backgroundColor = '';
-  });
+      .on('mouseover', function() {
+        this.style.backgroundColor = 'yellow';
+      })
+      .on('mouseleave', function() {
+        this.style.backgroundColor = '';
+      });
